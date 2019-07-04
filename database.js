c48a052227cea2583a7872be7f336f39d353b5c9
LiamKochNichol <49126926+LiamKochNichol@users.noreply.github.com>
2019-07-04 09:47:40 -0400

    Add files via upload

--- database.js
+++ database.js
@@ -2,17 +2,10 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('residents.db');
 
 db.serialize(() =>{
-  //start table
-  db.run("CREATE TABLE residents (name TEXT, floor TEXT, room INTEGER, route TEXT)");
-  //create mock residents
-  db.run("INSERT INTO residents VALUES ('John Smith', 'King', 13, 'Hello')");
-  db.run("INSERT INTO residents VALUES ('Jane Doe', 'Dundas', 58, 'Hello')");
-  db.run("INSERT INTO residents VALUES ('Mary Johnson', 'Queen', 37, 'Hello')");
-
-  db.each("SELECT name, room FROM residents", (err, row) => {
-    console.log(row.name + " @ " + row.room);
-
-  });
+  //start residents table
+  db.run("CREATE TABLE residents (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, floor TEXT, room INTEGER, route TEXT)");
+  //create table for floors
+  db.run("CREATE TABLE king (id INTEGER, room1 INTEGER, room2 INTEGER, room3 INTEGER, room4 INTEGER, room5 INTEGER, room6 INTEGER, FOREIGN KEY (id) REFERENCES residents(id))");
 });
 
 db.close();
--- server.js
+++ server.js
@@ -7,9 +7,118 @@ const PORT = process.env.PORT || 4001;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'Dementia Project')));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.get('/lastid', (req, res) => {
+  db.run('SELECT LAST_INSERT_ROWID()',
+    (err, id) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(id);
+        res.send(id);
+      }
+    }
+  )
+});
+
+app.post('/updateRoom', (req, res) => {
+  console.log(req.body.resId + " : " + req.body.room + " : " + req.body.routeSetting);
+  if (req.body.resId && req.body.room && req.body.routeSetting){
+    let query = 'UPDATE king SET ' + req.body.room + '= ($routeSetting) WHERE id = ($id)';
+    db.run(query,
+      {
+        $routeSetting: req.body.routeSetting,
+        $id: req.body.resId,
+      },
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    )
+  }
+});
+
+app.post('/king/inputResident', (req, res) => {
+    console.log("POST1");
+    console.log(req.body.name + req.body.room);
+    if (req.body.name && req.body.room) {
+      db.run(
+        'INSERT INTO residents (name, floor, room, route) VALUES ($name, "King", $room, "Hello")',
+        {
+          $name: req.body.name,
+          $room: req.body.room,
+        },
+        (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            db.run(
+              'INSERT INTO king (id, room1, room2, room3, room4, room5, room6) VALUES (LAST_INSERT_ROWID(), 0, 0, 0, 0, 0, 0)',
+              (err) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  db.get('SELECT id FROM residents WHERE name = $name AND room = $room',
+                    {
+                      $name: req.body.name,
+                      $room: req.body.room,
+                    },
+                    (err, row) => {
+                      if (err) {
+                        console.log(err);
+                        res.send("Error from insert");
+                      } else {
+                        console.log(row.id.toString());
+                        res.status(200).send((row.id).toString());
+                      }
+                    }
+                  )
+                }
+              }
+            )
+          }
+        }
+      )
+    } else {
+      console.log('Empty post');
+    }
+    console.log("GET 1");
+});
+
+app.get('/king/getit/:id', (req, res) => {
+  //find the id for the new resident
+  let resId = req.params.id;
+  db.all('SELECT * FROM king WHERE id = $id',
+    {
+      $id: resId,
+    },
+    (err, rows) => {
+      if (err) {
+        console.log("Route Error");
+      } else {
+       console.log(rows);
+       res.send(rows[0]);
+      }
+    }
+  )
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/node_modules/jquery-mapael/js/jquery.mapael.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/node_modules/jquery-mapael/js/jquery.mapael.js'));
+});
+
+app.get('/node_modules/raphael/raphael.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/node_modules/raphael/raphael.js'));
+});
+
+app.get('/node_modules/jquery-mapael/js/maps/newtestfloorplan.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/node_modules/jquery-mapael/js/maps/newtestfloorplan.js'));
 });
 
 app.get('/king', (req, res) => {
@@ -48,37 +157,36 @@ app.get('/king/:name', (req, res) => {
   );
 });
 
-app.post('/king', (req, res) => {
-  if (req.body.name && req.body.room) {
+app.post('/king/delete', (req, res) => {
+  resId = req.body.id;
+  if (resId) {
     db.run(
-      'INSERT INTO residents VALUES ($name, "King", $room, "Hello")',
+      'DELETE FROM residents WHERE id = ($id)',
       {
-        $name: req.body.name,
-        $room: req.body.room
+        $id: resId,
       },
-      (err) => {
+      (err, rows) => {
         if (err) {
-          res.send({message: 'error in app.post(/king)'});
+          console.log(err);
+          res.send(500);
         } else {
-          console.log('Post Error');
+          db.run(
+            'DELETE FROM king where id = ($id)',
+            {
+              $id: resId,
+            },
+            (err) => {
+              if (err) {
+                console.log(err);
+                res.status(500);
+              } else {
+                res.status(200);
+              }
+            }
+          )
         }
       }
-    )
-  } else {
-    console.log('Empty post')
-  }
-});
-
-app.post('/king/delete', (req, res) => {
-  resName = req.body.name;
-  resRoom = req.body.room;
-  if (resName) {
-    db.run(
-      'DELETE FROM residents WHERE name = ($name), room = ($room)',
-      {
-        $name: resName,
-        $room: resRoom
-      });
+    );
     res.send('Delete Success');
   } else {
     res.send("Error of request");
@@ -90,10 +198,6 @@ app.post('/king/edit/:name', (req, res) => {
   oldRoom = req.body.oldRoom;
   newName = req.body.newName;
   newRoom = req.body.newRoom;
-  console.log("Old Name: " + oldName);
-  console.log("New Name: " + newName);
-  console.log("Old Room: " + oldRoom);
-  console.log("New Room: "+ newRoom);
   if (oldName && oldRoom && newName && newRoom) {
     db.run(
       'UPDATE residents SET name = ($newName), room = ($newRoom) WHERE name = ($oldName) AND room = ($oldRoom)',
