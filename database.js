a7e08aa3ed2646ed9730d27aa8205ae76f453cda
LiamKochNichol <49126926+LiamKochNichol@users.noreply.github.com>
2019-08-08 16:52:20 -0400

    Add files via upload

--- database.js
+++ database.js
@@ -2,10 +2,12 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('residents.db');
 
 db.serialize(() =>{
+  //create nursing home table
+  db.run("CREATE TABLE home (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)");
+  //create floor tracker table
+  db.run("CREATE TABLE floors (homeId INTEGER, floorName TEXT, mapfile TEXT)");
   //start residents table
-  db.run("CREATE TABLE residents (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, floor TEXT, room INTEGER, route TEXT)");
-  //create table for floors
-  db.run("CREATE TABLE king (id INTEGER, room1 INTEGER, room2 INTEGER, room3 INTEGER, room4 INTEGER, room5 INTEGER, room6 INTEGER, FOREIGN KEY (id) REFERENCES residents(id))");
+  db.run("CREATE TABLE residents (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, floor TEXT, room INTEGER, homeId INTEGER)");
 });
 
 db.close();
--- server.js
+++ server.js
@@ -8,24 +8,209 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'Dementia Project')));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/static', express.static('public'));
 
-app.get('/lastid', (req, res) => {
-  db.run('SELECT LAST_INSERT_ROWID()',
-    (err, id) => {
+// *This code contains routers to all needed pages*
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/floors', (req, res) => {
+  res.sendFile(path.join(__dirname, '/floorDirect.html'));
+});
+
+app.get('/thisFloor', (req, res) => {
+  res.sendFile(path.join(__dirname, '/thisFloor.html'));
+});
+
+app.get('/setup', (req, res) => {
+  res.sendFile(path.join(__dirname, '/setup.html'));
+});
+
+// *This code serves images*
+//front logo
+app.get('/images/frontlogo.png', (req, res) => {
+  res.sendFile(path.join(__dirname, '/images/frontlogo.png'));
+});
+
+// *This code serves css*
+app.get('/stylesheet.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/stylesheet.css'));
+});
+
+//routers to files needed for Mapael Functionality
+app.get('/node_modules/jquery-mapael/js/jquery.mapael.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/node_modules/jquery-mapael/js/jquery.mapael.js'));
+});
+
+app.get('/node_modules/raphael/raphael.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/node_modules/raphael/raphael.js'));
+});
+
+app.get('/node_modules/jquery-mapael/js/maps/newtestfloorplan.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/node_modules/jquery-mapael/js/maps/newtestfloorplan.js'));
+});
+
+//*This code contains all editing functions for nursing homes and rooms*
+
+//this inserts a new home into "home" table with the given name and password
+app.post('/submitHome', (req, res) => {
+  let name = req.body.name;
+  let password = req.body.password;
+  db.run(
+    'INSERT INTO home (name, password) VALUES ($name, $password)',
+    {
+      $name: name,
+      $password: password,
+    },
+    (err) => {
       if (err) {
         console.log(err);
+        res.status(400).send({});
       } else {
-        console.log(id);
-        res.send(id);
+        res.status(200).send({});
       }
     }
   )
 });
 
-app.post('/updateRoom', (req, res) => {
-  console.log(req.body.resId + " : " + req.body.room + " : " + req.body.routeSetting);
+//for creating a new floor
+app.post('/submitFloor', (req, res) => {
+  //sort required information and increase readability
+  let home = req.body.homeName;
+  let password = req.body.homePass;
+  let name = req.body.floorName;
+  let start = req.body.start;
+  let stop = req.body.stop;
+  let mapfile = req.body.mapfile;
+  //first get the id of the nursing home
+  db.get('SELECT id FROM home WHERE name = ($home) AND password = ($password)',
+    {
+      $home: home,
+      $password: password,
+    },
+    (err, row) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send({})
+      } else if (row) {
+        //then create a row of info in the floor table
+        db.run('INSERT INTO floors (homeId, floorName, mapfile) VALUES ($id, $name, $mapfile)',
+          {
+            $id: row.id,
+            $name: name,
+            $mapfile: mapfile,
+          },
+          (err) => {
+            if (err) {
+              console.log(err);
+              res.status(400).send({})
+            } else {
+              let accum = 'id INTEGER, ';
+              for (i = start; i <= stop; i++) {
+                if (i === start) {
+                  accum += "room" + i + " INTEGER DEFAULT 0";
+                } else {
+                  accum += ", room" + i + " INTEGER DEFAULT 0";
+                }
+              }
+              let create = "CREATE TABLE " +  home + name + " (" + accum + ")";
+              //then create the floor table which is separate
+              db.run(create,
+                (err) => {
+                  if (err) {
+                    console.log(err);
+                    res.status(400).send({})
+                  } else {
+                    res.status(200).send({})
+                  }
+                }
+              )
+            }
+          }
+        )
+      } else {
+        console.log("ID is not found");
+      }
+    }
+  )
+});
+
+// *This code contains all functions managing floor information*
+
+//used for logging in
+app.get('/login', (req, res) => {
+  //input of home and password
+  //if they match in databse, send through
+  let homeName = req.query.home;
+  let homePass = req.query.pass;
+  db.get('SELECT id FROM home WHERE name = ($name) AND password = ($password)',
+    {
+      $name: homeName,
+      $password: homePass,
+    },
+    (err, id) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send({pass: "no"});
+      } else {
+        if (id) {
+          res.status(200).send({
+            pass: "yes",
+            id: id
+          });
+        } else {
+          res.status(400).send({});
+        }
+      }
+    }
+  )
+});
+
+//used to get the floors for a specific nursing home after login
+app.get('/getFloors', (req, res) => {
+  //input is homeId, return all floors of that home
+  let id = req.query.id;
+  db.all("SELECT * FROM floors WHERE homeId = ($id)",
+    {
+      $id: id,
+    },
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("Failed Query");
+      } else {
+        console.log(rows);
+        res.status(200).send(rows);
+      }
+    }
+  )
+});
+
+// *This code manages all resident functions*
+
+//this returns all the alert states of each room of the resident's floor
+app.get('/mapIt/:id/:floor', (req, res) => {
+  //input is resident id and the floor they are on
+  //output is the rooms and the alert state they have (alert on or off)
+  let query = 'SELECT * FROM ' + req.params.floor + ' WHERE id = ' + req.params.id;
+  db.all(query,
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.status(400);
+      } else {
+       res.status(200).send(rows[0]);
+      }
+    }
+  )
+});
+
+//this updates the alert states of each room for a specific resident on a specific floor
+app.post('/updateRoom/:floor', (req, res) => {
+  //input resident id, room to change state of, what state room will be in and what floor the room is on
   if (req.body.resId && req.body.room && req.body.routeSetting){
-    let query = 'UPDATE king SET ' + req.body.room + '= ($routeSetting) WHERE id = ($id)';
+    let query = 'UPDATE ' + req.params.floor + ' SET ' + req.body.room + '= ($routeSetting) WHERE id = ($id)';
     db.run(query,
       {
         $routeSetting: req.body.routeSetting,
@@ -34,32 +219,74 @@ app.post('/updateRoom', (req, res) => {
       (err) => {
         if (err) {
           console.log(err);
+          res.send(400).send({});
+        } else {
+          res.status(200).send({});
         }
       }
     )
   }
 });
 
-app.post('/king/inputResident', (req, res) => {
-    console.log("POST1");
-    console.log(req.body.name + req.body.room);
+//finds all residents on a particular floor
+app.get('/getFloorResidents/:tableName', (req, res) => {
+  //input floor name and return all residents on floor
+  db.all('SELECT * FROM residents WHERE floor = ($tableName)',
+  {
+    $tableName: req.params.tableName,
+  },
+  (err, rows) => {
+    res.json(rows);
+  });
+});
+
+//find all informatino on one resident
+app.get('/requestResidentInfo/:resId', (req, res) => {
+  //input resident id and return all information
+  const resId = req.params.resId;
+  db.all(
+  'SELECT * FROM residents WHERE id = $id',
+    {
+      $id: resId,
+    },
+    (err, rows) => {
+      if (rows.length > 0) {
+        res.send(rows[0]);
+      } else {
+        res.send({});
+      }
+    }
+  );
+});
+
+//creates a resident on a floor
+app.post('/inputResident/:tableName', (req, res) => {
+    //input is resident name, room number, home id and floor (toblename)
     if (req.body.name && req.body.room) {
+      //first insert data into the residents table
       db.run(
-        'INSERT INTO residents (name, floor, room, route) VALUES ($name, "King", $room, "Hello")',
+        'INSERT INTO residents (name, floor, room, homeId) VALUES ($name, $tableName, $room, $homeId)',
         {
           $name: req.body.name,
+          $tableName: req.params.tableName,
           $room: req.body.room,
+          $homeId: req.body.homeId,
         },
         (err) => {
           if (err) {
             console.log(err);
+            res.status(400);
           } else {
+            //then insert a row into the resident's floor
+            let query = 'INSERT INTO ' + req.params.tableName + ' (id) VALUES ( LAST_INSERT_ROWID() )'
             db.run(
-              'INSERT INTO king (id, room1, room2, room3, room4, room5, room6) VALUES (LAST_INSERT_ROWID(), 0, 0, 0, 0, 0, 0)',
+              query,
               (err) => {
                 if (err) {
                   console.log(err);
+                  res.status(400);
                 } else {
+                  //then select the resient id and return it
                   db.get('SELECT id FROM residents WHERE name = $name AND room = $room',
                     {
                       $name: req.body.name,
@@ -68,7 +295,7 @@ app.post('/king/inputResident', (req, res) => {
                     (err, row) => {
                       if (err) {
                         console.log(err);
-                        res.send("Error from insert");
+                        res.status(400);
                       } else {
                         console.log(row.id.toString());
                         res.status(200).send((row.id).toString());
@@ -84,82 +311,15 @@ app.post('/king/inputResident', (req, res) => {
     } else {
       console.log('Empty post');
     }
-    console.log("GET 1");
 });
 
-app.get('/king/getit/:id', (req, res) => {
-  //find the id for the new resident
-  let resId = req.params.id;
-  db.all('SELECT * FROM king WHERE id = $id',
-    {
-      $id: resId,
-    },
-    (err, rows) => {
-      if (err) {
-        console.log("Route Error");
-      } else {
-       console.log(rows);
-       res.send(rows[0]);
-      }
-    }
-  )
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.get('/node_modules/jquery-mapael/js/jquery.mapael.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '/node_modules/jquery-mapael/js/jquery.mapael.js'));
-});
-
-app.get('/node_modules/raphael/raphael.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '/node_modules/raphael/raphael.js'));
-});
-
-app.get('/node_modules/jquery-mapael/js/maps/newtestfloorplan.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '/node_modules/jquery-mapael/js/maps/newtestfloorplan.js'));
-});
-
-app.get('/king', (req, res) => {
-  res.sendFile(path.join(__dirname, '/king.html'));
-});
-
-app.get('/queen', (req, res) => {
-  res.sendFile(path.join(__dirname, '/queen.html'));
-});
-
-app.get('/dundas', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dundas.html'));
-});
-
-app.get('/king/residents', (req, res) => {
-  db.all('SELECT * FROM residents WHERE floor = "King"', (err, rows) => {
-    res.json(rows);
-  });
-});
-
-app.get('/king/:name', (req, res) => {
-  const lookupName = req.params.name;
-  db.all(
-  'SELECT * FROM residents WHERE name = $name AND floor = "King"',
-    {
-    $name: lookupName,
-    },
-    (err, rows) => {
-      if (rows.length > 0) {
-        console.log(rows);
-        res.send(rows[0]);
-      } else {
-        res.send({});
-      }
-    }
-  );
-});
-
-app.post('/king/delete', (req, res) => {
-  resId = req.body.id;
+//delete a resident from database
+app.post('/deleteResident', (req, res) => {
+  //input resident id and floor (tableName) and delete from database
+  let resId = req.body.id;
+  let tableName = req.body.tableName;
   if (resId) {
+    //first delete from residents table
     db.run(
       'DELETE FROM residents WHERE id = ($id)',
       {
@@ -168,32 +328,31 @@ app.post('/king/delete', (req, res) => {
       (err, rows) => {
         if (err) {
           console.log(err);
-          res.send(500);
+          res.status(400);
         } else {
-          db.run(
-            'DELETE FROM king where id = ($id)',
-            {
-              $id: resId,
-            },
+          //then delete from their respective floor table
+          let query = 'DELETE FROM ' + tableName + ' WHERE id = ' + resId;
+          db.run(query,
             (err) => {
               if (err) {
                 console.log(err);
-                res.status(500);
+                res.status(400);
               } else {
-                res.status(200);
+                res.status(200).send('Delete Success');
               }
             }
           )
         }
       }
     );
-    res.send('Delete Success');
   } else {
-    res.send("Error of request");
+    res.send({});
   }
 });
 
-app.post('/king/edit/:name', (req, res) => {
+//to edit the data of a resident
+app.post('/editResident', (req, res) => {
+  //input new and old - name and room to update database
   oldName = req.body.oldName;
   oldRoom = req.body.oldRoom;
   newName = req.body.newName;
@@ -205,14 +364,24 @@ app.post('/king/edit/:name', (req, res) => {
         $newName: newName,
         $newRoom: newRoom,
         $oldName: oldName,
-        $oldRoom: oldRoom
-      });
-    res.send('Edit Success')
+        $oldRoom: oldRoom,
+      },
+      (err) => {
+        if (err) {
+          console.log('Editing error');
+          res.status(400).send({});
+        } else {
+          console.log('Editing success');
+          res.status(200).send({});
+        }
+      }
+    );
   } else {
-    console.log("Edit error hit");
+    console.log("Editing error: Parameters are not properly set");
   }
 });
 
+//to start server
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
